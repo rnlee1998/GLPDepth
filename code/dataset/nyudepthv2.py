a5d50fb2b8f60f234ec9ea5f014bda1cea106a6a
rnlee1998 <1050145004@qq.com>
2022-02-17 08:54:40 +0800

    nyu train

--- code/dataset/nyudepthv2.py
+++ code/dataset/nyudepthv2.py
@@ -12,7 +12,7 @@ class nyudepthv2(BaseDataset):
         self.scale_size = scale_size
 
         self.is_train = is_train
-        self.data_path = os.path.join(data_path, 'nyu_depth_v2/sync')
+        self.data_path = os.path.join(data_path, 'nyu_depth_v2')
 
         self.image_path_list = []
         self.depth_path_list = []
@@ -20,6 +20,7 @@ class nyudepthv2(BaseDataset):
         txt_path = os.path.join(filenames_path, 'nyudepthv2')
         if is_train:
             txt_path += '/train_list.txt'
+            self.data_path = self.data_path + '/sync/'
         else:
             txt_path += '/test_list.txt'
             self.data_path = self.data_path + '/official_splits/test/'
@@ -38,7 +39,7 @@ class nyudepthv2(BaseDataset):
         filename = img_path.split('/')[-2] + '_' + img_path.split('/')[-1]
 
         image = cv2.imread(img_path)
-        #print("debug:",img_path)
+        #print("debug:",self.data_path,'/official_splits/test/',self.filenames_list[idx].split(' ')[1])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         depth = cv2.imread(gt_path, cv2.IMREAD_UNCHANGED).astype('float32')
 
